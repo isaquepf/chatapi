4b87855cb88de3e8abd78dca5b8ffde5382d3a20
Isaque Prestes Ferreira <isaquepferreira@hotmail.com>
2017-09-17 14:14:44 -0300

    c

--- public/client.js
+++ public/client.js
@@ -6,8 +6,8 @@
   }
 
   // support staging
-  if (document.domain === 'express-js-chat-demo-complete.herokuapp.com') {
-    apiDomain = 'https://express-js-chat-demo-complete.herokuapp.com';
+  if (document.domain === 'chatcscapi.herokuapp.com') {
+    apiDomain = 'https://chatcscapi.herokuapp.com';
   }
 
   var chatClient = angular.module('simple-chat-client', []);
@@ -18,61 +18,63 @@
     password: '123456'
   });
 
-  chatClient.controller('MainCtrl', function($scope, $http, auth, API_URL, CREDENTIALS) {
+  chatClient.controller('MainCtrl', function ($scope, $http, auth, API_URL, CREDENTIALS) {
     auth.login(CREDENTIALS.username, CREDENTIALS.password);
 
     $scope.rooms = [];
-    $http.get(API_URL + '/rooms').then(function(response) {
+    $http.get(API_URL + '/rooms').then(function (response) {
       $scope.rooms = response.data;
     });
-    $scope.select = function(room) {
+    $scope.select = function (room) {
       $scope.selectedRoom = room;
     };
   });
 
-  chatClient.controller('RoomCtrl', function($scope, $http, API_URL, API_DOMAIN) {
+  chatClient.controller('RoomCtrl', function ($scope, $http, API_URL, API_DOMAIN) {
     var socket = null;
 
     $scope.messages = [];
     $scope.roomId = null;
     $scope.newMessage = "";
-    $scope.$parent.$watch('selectedRoom', function(value){
+    $scope.$parent.$watch('selectedRoom', function (value) {
       $scope.roomId = value._id;
       $scope.getMessages();
       $scope.listen();
     });
 
-    $scope.getMessages = function() {
-      $http.get(API_URL + '/rooms/' + $scope.roomId + '/messages').then(function(response) {
+    $scope.getMessages = function () {
+      $http.get(API_URL + '/rooms/' + $scope.roomId + '/messages').then(function (response) {
         $scope.messages = response.data;
       });
     };
 
-    $scope.postMessage = function() {
+    $scope.postMessage = function () {
       $http.post(API_URL + '/rooms/' + $scope.roomId + '/messages', {
         text: $scope.newMessage
-      }).then(function(response){
+      }).then(function (response) {
         $scope.newMessage = null;
       });
     };
 
-    $scope.removeMessage = function(message) {
+    $scope.removeMessage = function (message) {
       $http.delete(API_URL + '/rooms/' + $scope.roomId + '/messages/' + message._id);
     };
 
-    $scope.listen = function() {
-      if(socket) {
+    $scope.listen = function () {
+      if (socket) {
         socket.disconnect();
       }
       console.log(API_DOMAIN)
       socket = io.connect(API_DOMAIN);
-      socket.emit("subscribe", { room: $scope.roomId });
-      socket.on('new message', function(message) {
+      socket.emit("subscribe", {
+        room: $scope.roomId
+      });
+      socket.on('new message', function (message) {
         $scope.messages.unshift(message);
         $scope.$apply();
       });
-      socket.on('message removed', function(data) {
-        _.remove($scope.messages, function(message) {
+      socket.on('message removed', function (data) {
+        _.remove($scope.messages, function (message) {
           return message._id === data.messageId;
         });
         $scope.$apply();
